0ee565c507c083574ee2c330b7013ab3a047ecd4
Julia Gafina <y28yulia@gmail.com>
2021-12-15 00:37:30 +0500

    Migrating to .net6

---
+++ ExampleGame/MyScene.cs
@@ -0,0 +1,47 @@
+﻿using Nubico.GameBase;
+using Nubico.Objects;
+using SFML.Graphics;
+using SFML.System;
+
+namespace ExampleGame
+{
+    public class MyScene : GameScene
+    {
+        private Mario mario;
+        private Enemy enemy;
+        private TextObject scoreText;
+        private int score = 0;
+        public MyScene(int text)
+        {
+            var helloText = new HelloText("Hello World!", 0, 50);
+            helloText.Size = 30;
+            helloText.Position = new Vector2f(Game.Width / 2, 50);
+            helloText.SetColor(Color.Cyan);
+            AddToScene(helloText);
+
+            mario = new Mario(50, 230);
+            AddToScene(mario);
+
+            enemy = new Enemy(Game.Width / 2, 242);
+            AddToScene(enemy);
+
+            for (var i = 0; i < 15; i++)
+            {
+                AddToScene(new Ground(36 * i, 280));
+            }
+
+            scoreText = new TextObject("0", Game.Width / 2, 350);
+            scoreText.Size = 25;
+            AddToScene(scoreText);
+        }
+
+        public override void OnEachFrame()
+        {
+            if (mario.X <= enemy.X + 2 && mario.X >= enemy.X - 2)
+            {
+                score++;
+                scoreText.SetText(score);
+            }
+        }
+    }
+}
